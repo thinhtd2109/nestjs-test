a3646790869152d828267d743e2ee4a8bcfe873a
Duc Thinh <thinh.tranduc@digipaysolution.com>
2023-07-24 17:41:31 +0700

    handle user

--- src/users/user.module.ts
+++ src/users/user.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './user.controller';
 import { UsersService } from './user.service';
+import moment from 'moment';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, { provide: 'MomentWrapper', useValue: moment }],
 })
 export class PostsModule { }
